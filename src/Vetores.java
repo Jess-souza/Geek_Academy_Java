66738cbf87883a50cc82ec3a49eac81c253b30aa
jess-souza <jsouzasouza120@gmail.com>
2021-12-07 20:38:38 -0300

    Vetores parte 1

---
+++ src/Vetores.java
@@ -0,0 +1,35 @@
+//Vetores parte 1
+
+//Arrays (vetores uni-dimensionais)
+
+/*
+float valores[10]; // 0...9
+int numeros [5]; // 0...4 (n-1) 5-1 = 4
+
+//Vetor [Indice];
+numeros[0]= 1; // primeiro elemento
+numeros[1]= 3; // segundo elemento
+numeros[2]= 5; // terceiro elemento
+numeros[3]= 7; // quarto elemento
+numeros[4]= 9; // quinto elemento
+
+*/
+
+public class Vetores {
+    public static void main(String[] args) {
+        //Declarar um vetor
+        int vetor[];
+
+        //Declarar e especificar o tamanho do vetor
+        int numeros[] = new int [6];
+
+        //Declarar, especificar o tamanho e inicializar
+        int outros_numeros[] = {1, 2, 6, 7, 8, 9};
+        
+        float valores[] = new float [6];
+        char caracteres[] = new char [20];
+        String nomes[] = new String [4];    
+
+        String frutas[] = {"Manga", "Morango", "Uva", "Banana"};
+    }
+}
